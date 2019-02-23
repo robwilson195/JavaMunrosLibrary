879e214320a44ae34561b65d586a2124cb02ddf0
Rob Wilson <rob.wilson195@gmail.com>
2019-02-23 22:14:33 +0000

    Successfully imported data from csv file as a list of lists of strings.

--- src/main/java/MunroLibrary.java
+++ src/main/java/MunroLibrary.java
@@ -1,4 +1,8 @@
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 public class MunroLibrary {
 
@@ -9,22 +13,53 @@ public class MunroLibrary {
 
         this.csvFileName = fileName;
         this.munros = new ArrayList<>();
-        this.munros = getData();
+//        this.munros = getData();
 
 
     }
 
-    public ArrayList<Munro> getData(){
+    public void getData(){
 
 
+        String pathName = this.csvFileName;
+        List<List<String>> csvData = new ArrayList<>();
+        try (Scanner scanner = new Scanner(new File("./" + this.csvFileName));) {
+            while (scanner.hasNextLine()) {
+                csvData.add(getRecordFromLine(scanner.nextLine()));
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
 
-        ArrayList<Munro> munroData;
-        return munroData;
+//        The following was used while experimenting with the results from the csv file.
+//        System.out.println(csvData.get(1).get(0));
+//        System.out.println(csvData.get(1).get(8));
+//        System.out.println(csvData.get(1));
+//        System.out.println(csvData.get(8));
+//        System.out.println("category "+ csvData.get(8).get(18));
+//        System.out.println("no category " + csvData.get(8).get(19));
+//        System.out.println("post 1997 " + csvData.get(8).get(28));
+
+        
+
     }
+
+    private List<String> getRecordFromLine(String line) {
+        List<String> values = new ArrayList<String>();
+        try (Scanner rowScanner = new Scanner(line)) {
+            rowScanner.useDelimiter(",");
+            while (rowScanner.hasNext()) {
+                values.add(rowScanner.next());
+            }
+        }
+        return values;
+        }
 
     public ArrayList<Munro> getMunros() {
         return munros;
     }
 
-
+    public String getCsvFileName() {
+        return csvFileName;
+    }
 }
--- src/test/java/MunroLibraryTest.java
+++ src/test/java/MunroLibraryTest.java
@@ -1,4 +1,5 @@
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
@@ -17,9 +18,21 @@ public class MunroLibraryTest {
 //        munro2 = new Munro("Auld Hill", 1589.7, "MUN", "NN62485");
 //        munro3 = new Munro("Zebra Mountain", 3967.4, "TOP", "NN23567");
 //        munro4 = new Munro("Donniegall Rise", 3967.1, "TOP", "NN56789");
-        munroLibrary = new MunroLibrary("munrotab");
+        munroLibrary = new MunroLibrary("munrotab.csv");
     }
 
+    @Test
+    public void importDataExperiment() {
+        munroLibrary.getData();
+    }
+
+    @Ignore
+    @Test
+    public void libraryHasCsvFileName() {
+        assertEquals("munrotab.csv", munroLibrary.getCsvFileName());
+    }
+
+    @Ignore
     @Test
     public void libraryHasRelevantData() {
         assertEquals("Ben Chonzie", munroLibrary.getMunros().get(0).getName());
